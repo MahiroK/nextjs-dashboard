30350a2347f298dbf7624f029b43fb915bb19dc9
MahiroK <a1r6j37it@outlook.jp>
2025-09-24 22:53:09 +0900

    fix: awaitの場所変更

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,9 @@ import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
 import { fetchRevenue } from "@/app/lib/data";
 
-const revenue = await fetchRevenue();
-
 export default async function Page() {
+  const revenue = await fetchRevenue();
+
   return (
     <main className="flex min-h-screen flex-col p-6">
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
